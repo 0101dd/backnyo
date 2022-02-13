74ee3dd979308abc689560b47c0eb36447cf2cb4
0101dd <aa09335665@gmail.com>
2022-02-13 15:03:53 +0800

    question, admin

--- controllers/questions.js
+++ controllers/questions.js
@@ -2,9 +2,23 @@ import questions from '../models/questions.js'
 
 export const createQuestion = async (req, res) => {
   try {
-    const result = await questions.create(req.body)
+    const result = await questions.create({ ...req.body })
     res.status(200).send({ success: true, message: '', result })
   } catch (error) {
-    console.log(error)
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      res.status(400).send({ success: false, message: error.errors[key].message })
+    } else {
+      res.status(500).send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+}
+
+export const getAllQuestions = async (req, res) => {
+  try {
+    const result = await questions.find()
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
   }
 }
--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import content from '../middleware/content.js'
-import { createQuestion } from '../controllers/questions.js'
+import auth from '../middleware/auth.js'
+import admin from '../middleware/admin.js'
+import {
+  createQuestion,
+  getAllQuestions
+} from '../controllers/questions.js'
 
 const router = express.Router()
 
-router.post('/', content('application/json'), createQuestion)
+router.post('/', auth, admin, content('application/json'), createQuestion)
+router.get('/all', auth, admin, getAllQuestions)
 
 export default router
